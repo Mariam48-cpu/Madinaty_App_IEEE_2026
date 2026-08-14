62ef530ea5fc199710471132394041acbb77ba7a
Mohamed Habib <mohamedmahmoudhabib2006@gmail.com>
2026-08-14 15:30:48 -0700

    Refactor code structure for improved readability and maintainability

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -13,21 +13,60 @@ abstract class AppAssets {
   static const String defaultAvatar = '$_imagesPath/default_avatar.png';
   static const String placePlaceholder = '$_imagesPath/place_placeholder.png';
   static const String coffeePlaceholder = '$_imagesPath/coffee_placeholder.png';
+  static const String locationImage = '$_imagesPath/location image.png';
+  static const String onboardingImage1 = '$_imagesPath/onbording image1.png';
+  static const String onboardingImage2 = '$_imagesPath/onbording image2.png';
+  static const String onboardingImage3 = '$_imagesPath/onbording image3.png';
 
   // --- Icons ---
-  static const String searchIcon = '$_iconsPath/ic_search.svg';
+  static const String animalIcon = '$_iconsPath/animal icon.svg';
+  static const String backIcon = '$_iconsPath/back icon.svg';
+  static const String bagIcon = '$_iconsPath/bag icon.svg';
+  static const String birthdayIcon = '$_iconsPath/birthday icon.svg';
+  static const String cafeLogoIcon = '$_iconsPath/cafe_logo icon.svg';
+  static const String dateIcon = '$_iconsPath/date icon.svg';
+  static const String digitalWalletIcon = '$_iconsPath/digital wallet icon.svg';
+  static const String emailIcon = '$_iconsPath/email icon.svg';
+  static const String favoriteIcon = '$_iconsPath/favorite icon.svg';
+  static const String friendsIcon = '$_iconsPath/friends icon.svg';
+  static const String homeIcon = '$_iconsPath/home icon.svg';
+  static const String listIcon = '$_iconsPath/list icon.svg';
+  static const String locationIcon = '$_iconsPath/location icon.svg';
+  static const String mapIcon = '$_iconsPath/map icon.svg';
+  static const String negativeIcon = '$_iconsPath/negative icon.svg';
+  static const String nileViewIcon = '$_iconsPath/nile_view icon.svg';
+  static const String noSoundIcon = '$_iconsPath/no_sound icon.svg';
+  static const String notificationIcon = '$_iconsPath/notification icon.svg';
+  static const String otpIcon = '$_iconsPath/otp icon.svg';
+  static const String outdoorIcon = '$_iconsPath/outdoor icon.svg';
+  static const String parkingIcon = '$_iconsPath/parking icon.svg';
+  static const String passwordIcon = '$_iconsPath/password icon.svg';
+  static const String paymentAtCafeIcon = '$_iconsPath/payment at cafe icon.svg';
+  static const String personIcon = '$_iconsPath/person icon.svg';
+  static const String plusIcon = '$_iconsPath/plus icon.svg';
+  static const String quietIcon = '$_iconsPath/quiet icon.svg';
+  static const String resetPasswordIcon = '$_iconsPath/reset_password icon.svg';
+  static const String searchIcon = '$_iconsPath/search icon.svg';
+  static const String shareIcon = '$_iconsPath/share icon.svg';
+  static const String specialtyCoffeeIcon = '$_iconsPath/specialty_coffee icon.svg';
+  static const String splashIcon = '$_iconsPath/splash icon.svg';
+  static const String studyIcon = '$_iconsPath/study icon.svg';
+  static const String toCheckoutIcon = '$_iconsPath/to_checkout icon.svg';
+  static const String wheelchairIcon = '$_iconsPath/wheelchair icon.svg';
+  static const String workIcon = '$_iconsPath/work icon.svg';
+
+  // --- Aliases for Navigation and Legacy Names ---
   static const String filterIcon = '$_iconsPath/ic_filter.svg';
-  static const String locationIcon = '$_iconsPath/ic_location.svg';
-  static const String notificationIcon = '$_iconsPath/ic_notification.svg';
-  static const String heartIcon = '$_iconsPath/ic_heart.svg';
-  static const String heartFilledIcon = '$_iconsPath/ic_heart_filled.svg';
+  static const String heartIcon = favoriteIcon;
+  static const String heartFilledIcon = favoriteIcon;
   static const String starIcon = '$_iconsPath/ic_star.svg';
-  static const String calendarIcon = '$_iconsPath/ic_calendar.svg';
+  static const String calendarIcon = dateIcon;
   static const String clockIcon = '$_iconsPath/ic_clock.svg';
 
   // --- Bottom Navigation Icons ---
-  static const String navHome = '$_iconsPath/ic_nav_home.svg';
-  static const String navMap = '$_iconsPath/ic_nav_map.svg';
-  static const String navLists = '$_iconsPath/ic_nav_lists.svg';
-  static const String navProfile = '$_iconsPath/ic_nav_profile.svg';
+  static const String navHome = homeIcon;
+  static const String navMap = mapIcon;
+  static const String navLists = listIcon;
+  static const String navProfile = personIcon;
 }
+
